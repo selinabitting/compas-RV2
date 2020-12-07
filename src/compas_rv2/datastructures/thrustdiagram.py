bc62ae41ff01bd3ce3408c2b0036d2b933a24c9c
Tom Van Mele <vanmelet@ethz.ch>
2020-12-07 22:58:44 +0100

    lumped stress

--- src/compas_rv2/datastructures/thrustdiagram.py
+++ src/compas_rv2/datastructures/thrustdiagram.py
@@ -46,28 +46,34 @@ class ThrustDiagram(FormDiagram):
         neighbors = self.vertex_neighbors(vertex)
         count = 0
         for nbr in neighbors:
-            t = sum(self.vertices_attribute('t', keys=[vertex, nbr])) / 2
-            f = self.edge_attribute((vertex, nbr), '_f')
+            edge_area = 0
+            edge_thickness = sum(self.vertices_attribute('t', keys=[vertex, nbr])) / 2
+            edge_force = self.edge_attribute((vertex, nbr), '_f')
+
+            if abs(edge_force) <= 0:
+                continue
+
             mp = self.edge_midpoint(vertex, nbr)
-            if abs(f) > 0:
-                edge_stress = 0
-                f0 = self.halfedge_face(vertex, nbr)
-                if f0 is not None:
-                    if self.face_attribute(f0, '_is_loaded'):
-                        f0_c = self.face_center(f0)
-                        area = length_vector(subtract_vectors(f0_c, mp)) * t
-                        if area > 0:
-                            edge_stress += f / area
-                f1 = self.halfedge_face(nbr, vertex)
-                if f1 is not None:
-                    if self.face_attribute(f1, '_is_loaded'):
-                        f1_c = self.face_center(f1)
-                        area = length_vector(subtract_vectors(f1_c, mp)) * t
-                        if area > 0:
-                            edge_stress += f / area
-                if edge_stress > 0:
-                    stress += edge_stress
-                    count += 1
+
+            f0 = self.halfedge_face(vertex, nbr)
+            if f0 is not None:
+                if self.face_attribute(f0, '_is_loaded'):
+                    f0_c = self.face_center(f0)
+                    area = length_vector(subtract_vectors(f0_c, mp)) * edge_thickness
+                    if area > 0:
+                        edge_area += area
+            f1 = self.halfedge_face(nbr, vertex)
+            if f1 is not None:
+                if self.face_attribute(f1, '_is_loaded'):
+                    f1_c = self.face_center(f1)
+                    area = length_vector(subtract_vectors(f1_c, mp)) * edge_thickness
+                    if area > 0:
+                        edge_area += area
+
+            if edge_area > 0:
+                stress += edge_force / edge_area
+                count += 1
+
         return stress / count
 
 
