0f844d8b612bee8f74fd5d42390b64b6a170bd39
Li <leochen1990@gmail.com>
2020-12-07 10:21:41 +0100

    clean up install.py

--- src/compas_rv2/install.py
+++ src/compas_rv2/install.py
@@ -1,24 +1,41 @@
 import compas
 import compas_rhino
 from compas_rhino.install import install
+from compas_rhino.uninstall import uninstall
 from compas_rhino.install_plugin import install_plugin
 from compas_rhino.uninstall_plugin import uninstall_plugin
 import argparse
 import os
 import json
 import sys
+import importlib
+
 
 PLUGIN_NAME = "RV2"
+PACKAGES = ['compas', 'compas_rhino', 'compas_tna', 'compas_cloud', 'compas_skeleton', 'compas_singular', 'compas_rv2']
+
+
+def is_editable(project_name):
+    """Is distribution an editable install?"""
+    for path_item in sys.path:
+        egg_link = os.path.join(path_item, "%s.egg-link" % project_name)
+        if os.path.isfile(egg_link):
+            return True
+    return False
+
 
 if __name__ == '__main__':
 
-    packages = ['compas', 'compas_rhino', 'compas_tna', 'compas_cloud', 'compas_skeleton', 'compas_singular', 'compas_rv2']
+    parser = argparse.ArgumentParser(
+    description='RhinoVault2 Installation command-line utility.')
 
-    import importlib
+    parser.add_argument('--remove_plugins', action='store_true', help="remove all existing plugins")
+    parser.add_argument('--remove_packages', action='store_true', help="remove all existing compas packages")
+    args = parser.parse_args()
 
     print("\n", "-"*10, "Checking packages", "-"*10)
 
-    for p in packages:
+    for p in PACKAGES:
         try:
             importlib.import_module(p)
         except ImportError:
@@ -27,13 +44,9 @@ if __name__ == '__main__':
         else:
             print('   {} {}'.format(p.ljust(20), "OK"))
 
-    parser = argparse.ArgumentParser(
-        description='RhinoVault2 Installation command-line utility.')
+    is_dev = is_editable("compas-RV2")
+    print("RV2 is editable install: ", is_dev)
 
-    parser.add_argument('--dev', action='store_true', help="install dev version of RV2 from current env")
-    parser.add_argument('--remove_plugins', action='store_true', help="remove all existing plugins")
-    parser.add_argument('--plugin_path', help="The path to the plugin directory.")
-    args = parser.parse_args()
 
     if args.remove_plugins:
         print("\n", "-"*10, "Removing existing plugins", "-"*10)
@@ -43,33 +56,26 @@ if __name__ == '__main__':
         for p in plugins:
             uninstall_plugin(p, version="6.0")
 
-    # print("\n", "-"*10, "Removing existing packages", "-"*10)
-    # uninstall()
+    if args.remove_packages:
+        print("\n", "-"*10, "Removing existing packages", "-"*10)
+        uninstall()
 
     print("\n", "-"*10, "Installing RV2 python plugin", "-"*10)
 
-    if args.dev:
-        rpy_plugin_path = os.path.dirname(__file__)
-        rpy_plugin_path = os.path.join(rpy_plugin_path, 'ui/Rhino/RV2')
-        rpy_plugin_path = os.path.abspath(rpy_plugin_path)
+    plugin_path = os.path.dirname(__file__)
+    plugin_path = os.path.join(plugin_path, 'ui/Rhino/RV2')
+    plugin_path = os.path.abspath(plugin_path)
 
-    elif args.plugin_path:
-        rpy_plugin_path = os.path.abspath(args.plugin_path)
-
+    if os.path.exists(plugin_path):
+        python_plugins_path = compas_rhino._get_python_plugins_path("6.0")
+        print("Plugin path:", python_plugins_path)
+        install_plugin(plugin_path, version="6.0")
     else:
-        rpy_plugin_path = os.path.dirname(__file__)
-        rpy_plugin_path = os.path.join(rpy_plugin_path, "..", "..", "..", "..", "..")
-        rpy_plugin_path = os.path.abspath(rpy_plugin_path)
-        print(rpy_plugin_path)
-
-    if os.path.exists(rpy_plugin_path):
-        install_plugin(rpy_plugin_path, version="6.0")
-    else:
-        raise RuntimeError("%s does not exist" % rpy_plugin_path)
+        raise RuntimeError("%s does not exist" % plugin_path)
 
     print("\n", "-"*10, "Installing COMPAS packages", "-"*10)
 
-    install(packages=packages)
+    install(packages=PACKAGES)
 
     print("\n", "-"*10, "Installation is successful", "-"*10)
 
@@ -85,22 +91,15 @@ if __name__ == '__main__':
     else:
         register_json = {"Plugins": {}, "Current": None}
 
-    if args.dev:
-        plugin_path = os.path.dirname(__file__)
-        plugin_path = os.path.join(plugin_path, "..", "..")
-        plugin_path = os.path.abspath(plugin_path)
-
-    else:
-        plugin_path = rpy_plugin_path
 
     plugin_info = {
-        "dev": args.dev,
+        "dev": is_dev,
         "path": plugin_path,
         "python": sys.executable,
         "packages": {},
     }
 
-    for name in packages:
+    for name in PACKAGES:
         package = importlib.import_module(name)
         plugin_info["packages"][name] = {"version": package.__version__}
 
