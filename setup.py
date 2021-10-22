6c2a1124282eb58d180b3f6d64409a984a94e72c
Licini <leochen1990@gmail.com>
2021-10-22 17:03:28 +0200

    Bump version to 1.3.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ optional_requirements = {}
 
 setup(
     name='compas-RV2',
-    version='1.3.1rc0',
+    version='1.3.1',
     description='RhinoVault for Rhino 6 based on COMPAS',
     long_description=long_description,
     long_description_content_type='text/markdown',
