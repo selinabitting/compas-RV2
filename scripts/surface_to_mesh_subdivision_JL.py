012cc9c8d219dc23e578d346c30837a65ebe8ba4
Juney Lee <juney.lee@arch.ethz.ch>
2021-11-06 08:30:50 +0100

    clean up comments in surface_to_mesh_subdivision and start incorporating non-quads

--- scripts/surface_to_mesh_subdivision_JL.py
+++ scripts/surface_to_mesh_subdivision_JL.py
@@ -26,13 +26,16 @@ from compas_rhino.artists import MeshArtist
 # ==============================================================================
 # 1. select rhino surface or polysurface
 # ==============================================================================
+
 guid = select_surface()
+rs.HideObjects(guid)
 
 
 # ==============================================================================
 # 2. convert selection to compas rhinosurface
-# rhinosurface could mean one or multiple joined surfaces ("polysurface")...
 # ==============================================================================
+
+# here, rhinosurface could mean one or multiple joined surfaces ("polysurface")... from_guid will take care of it automatically.
 rhinosurface = RhinoSurface.from_guid(guid)
 brep = Rhino.Geometry.Brep.TryConvertBrep(rhinosurface.geometry)
 
@@ -40,9 +43,10 @@ brep = Rhino.Geometry.Brep.TryConvertBrep(rhinosurface.geometry)
 # ==============================================================================
 #  3. make a Mesh from the rhinosurface, then draw it
 # ==============================================================================
+
 mesh = rhinosurface.to_compas(cleanup=False)
 coarse_mesh_artist = MeshArtist(mesh, layer='coarse_mesh')
-coarse_mesh_artist.draw_edges(color=(100, 100, 100))
+coarse_mesh_artist.draw_edges(color=(0, 0, 0))
 
 # select_surface() will already filter selection to surface or polysurface...
 #
@@ -57,40 +61,81 @@ coarse_mesh_artist.draw_edges(color=(100, 100, 100))
 
 
 # ==============================================================================
-#  4. get the u and v edges per face and keep track in faces_dict
+#  x. default subdivision numbers
 # ==============================================================================
 
-# faces_dict will store per face, the corresponding surface geometry/object, and the u and v edges... this can eventually be stored as face attributes in our eventual surfacemesh datastructure
+default_nu_nv = 10  # for quads
+default_n = 4  # for non-quads
+
+
+# ==============================================================================
+#  4. get the face geometry and u + v edges per face and keep track in faces_dict
+# ==============================================================================
+
+# faces_dict will store per face, the following info:
+#
+# 'is_quad' : boolean, whether a brep_face is quad or not
+# 'u_edge': u direction edge of the brep_face
+# 'v_edge': v direction edge of the brep_face
+# 'nu': subdivision number in the u direction
+# 'nv': subdivision number in the v direction
+# 'n': subdivision number for a non-quad (catmull clark subdivision count)
+# 'brep_face': the geometry of the corresponding surface
+#
+# faces_dict will eventually be stored as face attributes in our surfacemesh datastructure
 
 faces_dict = {}
 
 gkeys = {geometric_key(mesh.vertex_coordinates(vertex)): vertex for vertex in mesh.vertices()}
 
 for brep_face, face in zip(brep.Faces, mesh.faces()):
-    domain_u = brep_face.Domain(0)
-    domain_v = brep_face.Domain(1)
-    u0_xyz = brep_face.PointAt(domain_u[0], domain_v[0])
-    u1_xyz = brep_face.PointAt(domain_u[1], domain_v[0])
-    v0_xyz = brep_face.PointAt(domain_u[0], domain_v[0])
-    v1_xyz = brep_face.PointAt(domain_u[0], domain_v[1])
-    # we can remove the tuple() thing below ... this is from before when we din't know what format u0, u1, v0, v1 were in...
-    u0 = gkeys[geometric_key(u0_xyz)]
-    u1 = gkeys[geometric_key(u1_xyz)]
-    v0 = gkeys[geometric_key(v0_xyz)]
-    v1 = gkeys[geometric_key(v1_xyz)]
-    faces_dict[face] = {'u_edge': (u0, u1),
-                        'v_edge': (v0, v1),
-                        'brep_face': brep_face}
-    # we can skip storing the below information, as it will be available through the 'brep_face' item stored in the faces_dict[face]
-    # brep_surfaces_dic[brep_surface] = {'u0': u0,
-    #                                 'u1': u1,
-    #                                 'v0': v0,
-    #                                 'v1': v1}
+
+    face_info = {'is_quad': False,
+                 'u_edge': None,
+                 'v_edge': None,
+                 'nu': default_nu_nv,
+                 'nv': default_nu_nv,
+                 'n': default_n,
+                 'brep_face': brep_face}
+
+    # check whether the brep_face is a quad or not (we can think of a shorter, more elegant solution...)
+    loop = brep_face.OuterLoop
+    curve = loop.To3dCurve()
+    segments = curve.Explode()
+
+    if len(segments) == 4:  # if brep_face is a quad, determine uv info
+
+        domain_u = brep_face.Domain(0)
+        domain_v = brep_face.Domain(1)
+        u0_xyz = brep_face.PointAt(domain_u[0], domain_v[0])
+        u1_xyz = brep_face.PointAt(domain_u[1], domain_v[0])
+        v0_xyz = brep_face.PointAt(domain_u[0], domain_v[0])
+        v1_xyz = brep_face.PointAt(domain_u[0], domain_v[1])
+
+        # we can remove the tuple() thing below ... this is from before when we din't know what format u0, u1, v0, v1 were in...
+        u0 = gkeys[geometric_key(u0_xyz)]
+        u1 = gkeys[geometric_key(u1_xyz)]
+        v0 = gkeys[geometric_key(v0_xyz)]
+        v1 = gkeys[geometric_key(v1_xyz)]
+
+        face_info.update({'is_quad': True,
+                          'u_edge': (u0, u1),
+                          'v_edge': (v0, v1),
+                          'brep_face': brep_face})
+
+        # we can skip storing the below information, as it will be available through the 'brep_face' item stored in the faces_dict[face]
+        # brep_surfaces_dic[brep_surface] = {'u0': u0,
+        #                                 'u1': u1,
+        #                                 'v0': v0,
+        #                                 'v1': v1}
+
+    faces_dict[face] = face_info
 
 
 # ==============================================================================
 #  5. draw uv information per face (just for our visual reference)
 # ==============================================================================
+
 def draw_uv_vectors(mesh, faces_dict):
     lines = []
     for face in faces_dict:
@@ -106,11 +151,15 @@ def draw_uv_vectors(mesh, faces_dict):
     compas_rhino.draw_lines(lines, layer='uv_vectors', clear=False, redraw=False)
 
 
-# draw uv information per face
 draw_uv_vectors(mesh, faces_dict)
 
 
-# we can always start with a default subdivision number, then the user can decide by selecting edge strips, whether to change the default n division or not...
+# ==============================================================================
+#  x. our main surface subdivision functions
+# ==============================================================================
+
+# we can always start with a default subdivision number, then the user can decide by selecting edge strips, whether to change the default n division or not.
+
 """
 #--------- Get integer division number for U,V  ---------------
 
@@ -128,53 +177,74 @@ elif nu and nu > 2:
 """
 
 
-# ==============================================================================
-#  x. our main surface subdivision function
-# ==============================================================================
+# 1. for quads
+def subdivide_quad(brep_face, nu, nv):
+    """Subdivide a single quad brep_face"""
 
-# face_dict : has the surface geometry information, and the u and v edge information
-# nu_nv : has the information on the subdivision # for u and v directions
+    # unlike the previous version, we input a single brep_face
 
-def surface_to_uvmesh(faces_dict, nu_nv):
+    domain_u = brep_face.Domain(0)
+    domain_v = brep_face.Domain(1)
+
+    du = (domain_u[1] - domain_u[0]) / (nu)
+    dv = (domain_v[1] - domain_v[0]) / (nv)
+
+    def point_at(i, j):
+        return brep_face.PointAt(i, j)
+
+    quads = []
+    for i in range(nu):
+        for j in range(nv):
+            a = point_at(domain_u[0] + (i + 0) * du, domain_v[0] + (j + 0) * dv)
+            b = point_at(domain_u[0] + (i + 1) * du, domain_v[0] + (j + 0) * dv)
+            c = point_at(domain_u[0] + (i + 1) * du, domain_v[0] + (j + 1) * dv)
+            d = point_at(domain_u[0] + (i + 0) * du, domain_v[0] + (j + 1) * dv)
+            quads.append([a, b, c, d])
+
+    return Mesh.from_polygons(quads)
+
+
+# 2.  for non-quads
+def subdivide_nonquad(brep_face, n):
+    """subdivide a single non-quad brep_face"""
+
+    # this will be our modified version of catmull clark subdivision
+
+    pass
+
+
+# 1 + 2. combination
+
+# here, we don't actually need the mesh to run the function as it is written, but putting it in here anyway for now, as our eventual setup will all operate entirely around/on the mesh (which we will call the surfacemesh), and faces_dict will come from the face attributes of that mesh
+
+def subdivide_surfacemesh(mesh, faces_dict):
 
     subd_meshes = []
 
-    # iterates through all the individual surfaces and then divides it with the corresponding nu and nv values
-    for face in faces_dict:
+    for face in mesh.faces():
 
-        nu = nu_nv[face]['nu']
-        nv = nu_nv[face]['nv']
+        brep_face = faces_dict[face]['brep_face']
+        quad = faces_dict[face]['is_quad']
 
-        # the face (or the surface) information is retrieved from the faces_dict
-        face = faces_dict[face]['brep_face']
+        if quad:  # if face is a quad
+            nu = faces_dict[face]['nu']
+            nv = faces_dict[face]['nv']
 
-        domain_u = face.Domain(0)
-        domain_v = face.Domain(1)
+            quad_subd_mesh = subdivide_quad(brep_face, nu, nv)
+            subd_meshes.append(quad_subd_mesh)
 
-        du = (domain_u[1] - domain_u[0]) / (nu)
-        dv = (domain_v[1] - domain_v[0]) / (nv)
-
-        def point_at(i, j):
-            return face.PointAt(i, j)
-
-        quads = []
-        for i in range(nu):
-            for j in range(nv):
-                a = point_at(domain_u[0] + (i + 0) * du, domain_v[0] + (j + 0) * dv)
-                b = point_at(domain_u[0] + (i + 1) * du, domain_v[0] + (j + 0) * dv)
-                c = point_at(domain_u[0] + (i + 1) * du, domain_v[0] + (j + 1) * dv)
-                d = point_at(domain_u[0] + (i + 0) * du, domain_v[0] + (j + 1) * dv)
-                quads.append([a, b, c, d])
-
-        subd_meshes.append(Mesh.from_polygons(quads)) # this will create an entirely new mesh
+        else:  # if face is a non-quad
+            n = faces_dict[face['n']]
+            nonquad_subd_mesh = subdivide_nonquad(brep_face, n)
+            subd_meshes.append(nonquad_subd_mesh)
 
     return meshes_join(subd_meshes)
 
 
-# ------------------------------------------------------------------------------
+# ==============================================================================
 # x. edge subdivision
-# ------------------------------------------------------------------------------
-#
+# ==============================================================================
+
 # def subdivide_edge_strip(mesh, pick_edge, n):
 #
 # def mesh_split_edge(mesh, u, v, n=2):
@@ -186,18 +256,12 @@ def surface_to_uvmesh(faces_dict, nu_nv):
 #  6. subdivide surfaces with default subdivsion nu and nv
 # ==============================================================================
 
-default = 10
-nu_nv = {face: {'nu': default, 'nv': default} for face in mesh.faces()}
-# the nu_nv dictionary stores the information for nu and nv
-# nu: number of subdivision in the u direction
-# nv: number of subdivision in the v direction
-# this information can eventually be stored as face attributes of the surfacemesh datastructure
-
 # subdivide the face with default subdivision values
-subd1 = surface_to_uvmesh(faces_dict, nu_nv)
+subd1 = subdivide_surfacemesh(mesh, faces_dict)
 subd1_artist = MeshArtist(subd1, layer='subd1_mesh')
-subd1_artist.draw_mesh()
+subd1_artist.draw_edges(color=(130, 130, 130))
 rs.EnableRedraw(True)
+
 
 # ==============================================================================
 # steps 7 through 10 can be wrapped into an iterative/interactive loop
@@ -218,7 +282,8 @@ pick_edge = mesh_select_edge(mesh)
 #  8. get subdivision number
 # ==============================================================================
 # setting the minimum here will force users to enter a minimum integer of 2
-n = compas_rhino.rs.GetInteger('divide into?', minimum=2)
+nu_or_nv = compas_rhino.rs.GetInteger('divide into?', minimum=2)
+
 #if not n or n < 2:
 #    print('has to be larger than 2!!')
 #else:
@@ -228,15 +293,14 @@ n = compas_rhino.rs.GetInteger('divide into?', minimum=2)
 
 
 # ==============================================================================
-# instead of subdividing the strips, we can just update tne nu and nv information
-# then use it to run the surface_to_uvmesh again
+#  8. update nu or nv information for the faces of the edge_strip
 # ==============================================================================
 
-
-# given the picked edge, computer whether to divide u or v
+# get the faces of the edge_strip
 edge_strip = set(frozenset(edge) for edge in mesh.edge_strip(pick_edge))
 edge_strip_faces = set()
 
+# we will need to modify the edge_strip function to incorporate non-quads...
 for u, v in edge_strip:
     face1, face2 = mesh.edge_faces(u, v)
     if face1 is not None:
@@ -244,28 +308,39 @@ for u, v in edge_strip:
     if face2 is not None:
         edge_strip_faces.add(face2) # ?????? doesn't it add the same face twice?
 
-# determine whether the face's u or v domain is aligned with the edge strip
-# then update nu and nv directly in the nu_nv dictionary
+# update the nu or nv for quads, and n for non-quads
 for face in edge_strip_faces:
-    u_edge = faces_dict[face]['u_edge']
-    if frozenset(u_edge) in edge_strip:
-        nu_nv[face]['nu'] = n
-    else:
-        nu_nv[face]['nv'] = n
+    quad = faces_dict[face]['is_quad']
+
+    # the nu_or_nv and the the count for catmull clark subdivision will need to be coordinated...
+
+    if quad:  # if the face is a quad
+        u_edge = faces_dict[face]['u_edge']
+        if frozenset(u_edge) in edge_strip:
+            faces_dict[face]['nu'] = nu_or_nv
+        else:
+            faces_dict[face]['nv'] = nu_or_nv
+
+    else:  # if the face is a non-quad
+        faces_dict[face]['n'] = nu_or_nv
+
+
+# uncomment below to see results!
 
 
 # ==============================================================================
 #  9. subdivide the surfaces again with the updated nu_nv
 # ==============================================================================
-subd2 = surface_to_uvmesh(faces_dict, nu_nv)
+
+# subd2 = subdivide_surfacemesh(mesh, faces_dict)
 
 
 # ==============================================================================
 #  10. draw the newly subdivided mesh
 # ==============================================================================
-rs.HideObjects(guid)
-coarse_mesh_artist.clear_layer() # delete the coarse mesh
-subd1_artist.clear_layer() # delete the default subd mesh
 
-subd2_artist = MeshArtist(subd2, layer='subd2_mesh')
-subd2_artist.draw_mesh()
+# coarse_mesh_artist.clear_layer() # delete the coarse mesh
+# subd1_artist.clear_layer() # delete the default subd mesh
+
+# subd2_artist = MeshArtist(subd2, layer='subd2_mesh')
+# subd2_artist.draw_mesh()
