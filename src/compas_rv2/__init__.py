57102861da160a10778726b5edb1c6777e954e8a
Licini <leochen1990@gmail.com>
2021-11-17 16:30:26 +0100

    Bump version to 1.4.1rc0

--- src/compas_rv2/__init__.py
+++ src/compas_rv2/__init__.py
@@ -24,7 +24,7 @@ __author__ = ['tom van mele <van.mele@arch.ethz.ch>']
 __copyright__ = 'Block Research Group - ETH Zurich'
 __license__ = 'MIT License'
 __email__ = 'van.mele@arch.ethz.ch'
-__version__ = '1.4.0'
+__version__ = '1.4.1rc0'
 
 
 HERE = os.path.dirname(__file__)
